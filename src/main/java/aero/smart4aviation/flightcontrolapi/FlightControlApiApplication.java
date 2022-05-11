5b40e830c5bf8ff4b918985707232813e6a435fc
zonkje <szymon.radziszewski93@gmail.com>
2022-05-11 17:56:16 +0200

    initial project add. close #1

---
+++ src/main/java/aero/smart4aviation/flightcontrolapi/FlightControlApiApplication.java
@@ -0,0 +1,13 @@
+package aero.smart4aviation.flightcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aero/smart4aviation/flightcontrolapi/FlightControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aero.smart4aviation.flightcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
