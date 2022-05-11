63a98314cdaff05e094209f7915d38c20cf6f7be
zonkje <szymon.radziszewski93@gmail.com>
2022-05-11 21:02:09 +0200

    add Flight entity. close #2

---
+++ src/main/java/aero/smart4aviation/flightcontrolapi/model/Flight.java
@@ -0,0 +1,59 @@
+package aero.smart4aviation.flightcontrolapi.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "flight")
+public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "flight_id", nullable = false)
+    private Long flightId;
+
+    @Column(name = "flight_number")
+    private Long flightNumber;
+
+    @Column(name = "departure_airport_iata_code")
+    private String departureAirportIATACode;
+
+    @Column(name = "arrival_airport_iata_code")
+    private String arrivalAirportIATACode;
+
+    @Column(name = "departure_date")
+    private String departureDate;
+
+    @OneToMany(
+            mappedBy = "flight",
+            @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH})
+    )
+    Collection<Baggage> baggage;
+
+    @OneToMany(
+            mappedBy = "flight",
+            @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH})
+    )
+    Collection<Cargo> cargo;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Flight flight = (Flight) o;
+        return flightId != null && Objects.equals(flightId, flight.flightId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
